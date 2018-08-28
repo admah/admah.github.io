e25de92a630161ea3a6f246cbbb3161c0955d35e
Adam Murray <adam.murray@lifeway.com>
2018-08-28 00:07:27 -0500

    Update index page and styling.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import Header from '../components/header'
 import './index.css'
 
+const PageWrapper = styled.div`
+  font-family: 'Open Sans', sans-serif;
+`
+
+const ContentWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 2.2rem 1.45rem;
+  padding-top: 0;
+`
+
 const Layout = ({ children, data }) => (
-  <div>
+  <PageWrapper>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -15,17 +27,8 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+    <ContentWrapper>{children()}</ContentWrapper>
+  </PageWrapper>
 )
 
 Layout.propTypes = {
