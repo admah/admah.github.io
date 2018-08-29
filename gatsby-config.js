145888086f1727afebc590f1d563a1f2e8877015
Adam Murray <adam.murray@lifeway.com>
2018-08-29 12:21:04 -0500

    Add gatsby google fonts plugin to prevent fouc when fonts load. Remove from index.css.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: '',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `rock salt`,
+          `open sans\:300,400,600,700`, // you can also specify font weights and styles
+        ],
+      },
+    },
+  ],
 }
